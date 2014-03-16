97fa16e8008da7a344a3148dd6d60620d71a61f8
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-03-16 14:30:43 -0300

    Add generated Rails app
    This app was generated by:
    `rails new . --skip-bundle --skip-test-unit --skip-javascript`
    
    See https://github.com/gonzalo-bulnes/simple_token_authentication/\
        blob/fix-authentication-token-not-renewed-after-password-change/\
        features/step_definitions/dummy_app_steps.rb#L18

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsEngineDecoratorsDemo::Application.config.secret_key_base = 'ae8466b68b51d262b93d318247fcd595016b137b05e80f9c6b5c4375c88581c831679e5b6b08df38b6bc36cb354a91d33f5eb525075582ba2fd3f11717231463'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsEngineDecoratorsDemo::Application.config.session_store :cookie_store, key: '_rails_engine_decorators_demo_session'
