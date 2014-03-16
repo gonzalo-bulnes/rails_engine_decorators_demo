e96cdcf9f9015200317f1785a934c8493c21876d
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-03-16 15:09:55 -0300

    Add PrivatePostController decorator

---
+++ app/decorators/controllers/dummy/private_posts_controller_decorator.rb
@@ -0,0 +1,8 @@
+PrivatePostsController.class_eval do
+  # See https://github.com/atd/rails_engine_decorators/blob/\
+  # 8384cb7f79e8673a10c1638b9ad64b0c7f1cdad1/test/rails_engine_decorators_test.rb
+  def decorated?
+    true
+  end   
+end
+
