e6d3abacd88b698b8e2d61c11fe5dd844cc2a6df
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-03-16 15:05:14 -0300

    Ensure PrivatePostsController acts as token auth. handler for User

--- app/controllers/private_posts_controller.rb
+++ app/controllers/private_posts_controller.rb
@@ -1,14 +1,15 @@
 class PrivatePostsController < ApplicationController
+
+  acts_as_token_authentication_handler_for User
+
   before_action :set_private_post, only: [:show, :edit, :update, :destroy]
 
   # GET /private_posts
-  # GET /private_posts.json
   def index
     @private_posts = PrivatePost.all
   end
 
   # GET /private_posts/1
-  # GET /private_posts/1.json
   def show
   end
 
@@ -22,43 +23,29 @@ class PrivatePostsController < ApplicationController
   end
 
   # POST /private_posts
-  # POST /private_posts.json
   def create
     @private_post = PrivatePost.new(private_post_params)
 
-    respond_to do |format|
-      if @private_post.save
-        format.html { redirect_to @private_post, notice: 'Private post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @private_post }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @private_post.errors, status: :unprocessable_entity }
-      end
+    if @private_post.save
+      redirect_to @private_post, notice: 'Private post was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
   # PATCH/PUT /private_posts/1
-  # PATCH/PUT /private_posts/1.json
   def update
-    respond_to do |format|
-      if @private_post.update(private_post_params)
-        format.html { redirect_to @private_post, notice: 'Private post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @private_post.errors, status: :unprocessable_entity }
-      end
+    if @private_post.update(private_post_params)
+      redirect_to @private_post, notice: 'Private post was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
   # DELETE /private_posts/1
-  # DELETE /private_posts/1.json
   def destroy
     @private_post.destroy
-    respond_to do |format|
-      format.html { redirect_to private_posts_url }
-      format.json { head :no_content }
-    end
+    redirect_to private_posts_url, notice: 'Private post was successfully destroyed.'
   end
 
   private
@@ -67,8 +54,9 @@ class PrivatePostsController < ApplicationController
       @private_post = PrivatePost.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Only allow a trusted parameter "white list" through.
     def private_post_params
       params.require(:private_post).permit(:title, :body)
     end
 end
+
